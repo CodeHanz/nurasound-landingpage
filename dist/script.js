4925bb1a3ea0129d49f8cd5c15ae7fb4849acf5c
FARHAN <farhannagantara@gmail.com>
2024-12-28 14:37:01 +0700

    fixed navbar bug

--- dist/script.js
+++ dist/script.js
@@ -21,7 +21,7 @@ hamburger.addEventListener("click", function () {
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
+    // console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
